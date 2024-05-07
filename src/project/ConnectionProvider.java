5a2c9d7e32bcfe870fc75f5bcf52708c5ff5976b
Chetan Nerkar <141409575+Chetan-033@users.noreply.github.com>
2024-05-07 18:07:04 +0530

    Update ConnectionProvider.java

--- src/project/ConnectionProvider.java
+++ src/project/ConnectionProvider.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package project;
 import java.sql.*;
 
 /**
  *
- * @author 91814
+ * @author chetan_nerkar
  */
 public class ConnectionProvider {
     public static Connection getCon()
