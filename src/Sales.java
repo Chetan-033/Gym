7fcbddfb0fc92c6a885b0c93ac709a112bed96f8
Chetan Nerkar <141409575+Chetan-033@users.noreply.github.com>
2024-05-07 18:04:38 +0530

    first commit

--- src/Sales.java
+++ src/Sales.java
@@ -6,11 +6,6 @@ import java.util.Date;
 import javax.swing.JOptionPane;
 import project.ConnectionProvider;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
-
 /**
  *
  * @author chetan_nerkar
