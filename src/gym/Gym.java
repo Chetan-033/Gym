03bc6af3b9bc93e8a43ad4ab94d9af315055961f
Chetan Nerkar <141409575+Chetan-033@users.noreply.github.com>
2024-05-07 18:06:30 +0530

    first commit

--- src/gym/Gym.java
+++ src/gym/Gym.java
@@ -1,12 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package gym;
 
 /**
  *
- * @author 91814
+ * @author chetan_nerkar
  */
 public class Gym {
 
