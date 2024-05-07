0b604b5a07c79fe2a34dbcbb4eea1e751e281873
Chetan Nerkar <141409575+Chetan-033@users.noreply.github.com>
2024-05-07 18:05:07 +0530

    first commit

--- src/Login.java
+++ src/Login.java
@@ -2,10 +2,6 @@
 import java.awt.Color;
 import javax.swing.JOptionPane;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 
 /** 
  *
