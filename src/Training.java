8477bc08cbb1dfdd278a43d219b37c4f98efeaaa
Chetan Nerkar <141409575+Chetan-033@users.noreply.github.com>
2024-05-07 18:08:48 +0530

    Update

--- src/Training.java
+++ src/Training.java
@@ -2,10 +2,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import javax.swing.JOptionPane;
 import project.ConnectionProvider;
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 
 /**
  *
