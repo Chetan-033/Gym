0ccd983291e52789c19c6cc3ed599965824d47c7
Chetan Nerkar <141409575+Chetan-033@users.noreply.github.com>
2024-05-07 18:03:31 +0530

    first commit

--- src/Payment.java
+++ src/Payment.java
@@ -6,11 +6,6 @@ import javax.swing.JOptionPane;
 import project .ConnectionProvider;
 import java.util.Date;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
-
 /**
  *
  * @author chetan_nerkar
